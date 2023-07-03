b88db880510b97e357325747562c1cd59d9d956b
Rafael Bini <rfabini1996@gmail.com>
2023-07-03 09:23:31 -0300

    changed bat

--- startRunning.bat
+++ startRunning.bat
@@ -1,2 +1,15 @@
 @echo off
+
+git fetch >output.txt 2>&1
+
+for /f %%i in ("output.txt") do set size=%%~zi
+
+if %size% gtr 0 git reset --hard
+
+if %size% gtr 0 git pull origin master
+
+if %size% gtr 0 npm ci
+
+del output.txt
+
 start /B node "D:\dev\Richot\PrinterServer\runner.js"
